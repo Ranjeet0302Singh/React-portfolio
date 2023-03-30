24ceb82b1f5375db1914d733a548e64bf67f1313
Ranjeet Singh Rawat <ranjeet843507@gmail.com>
2023-03-31 01:08:24 +0530

    home page responsive done

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -23,6 +23,11 @@ const About = () => {
     <>
       <div className="container about-page page">
         <div className="text-zone">
+        <span className="tags top-tags">
+          <span className="top-tag-html">&lt;html&gt;</span>
+          <br />
+          <span>&lt;body&gt;</span>
+        </span>
           <h1>
             <AnimatedLetters
             letterClass={letterClass}
@@ -32,14 +37,18 @@ const About = () => {
           </h1>
           <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt iste saepe eum totam eius qui distinctio cupiditate odio, iure enim quidem veniam quo tempora, quam sint doloribus officiis iusto error?</p>
           <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt iste saepe eum totam eius qui distinctio cupiditate odio, iure enim quidem veniam quo tempora, quam sint doloribus officiis iusto error?</p>
-          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt iste saepe eum totam eius qui distinctio cupiditate odio, iure enim quidem veniam quo tempora, quam sint doloribus officiis iusto error?</p>
-          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt iste saepe eum totam eius qui distinctio cupiditate odio, iure enim quidem veniam quo tempora, quam sint doloribus officiis iusto error?</p>
-          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt iste saepe eum totam eius qui distinctio cupiditate odio, iure enim quidem veniam quo tempora, quam sint doloribus officiis iusto error?</p>
+    
         </div>
 
         <div className="textsphere">
           <Textsphere/>
         </div>
+
+        <span className="tags bottom-tags">
+          &lt;/body&gt;
+          <br />
+          <span className="bottom-tag-html">&lt;/html&gt;</span>
+        </span>
       </div>
       <Loader type="pacman" />
     </>
