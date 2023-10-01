e22e3f1999c83538c9d46f4a8aca9861acf302fc
Ranjeet Singh Rawat <ranjeet843507@gmail.com>
2023-10-01 23:19:21 +0530

    contact from working done

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,16 +1,115 @@
-import './Contact.scss'
-import React from 'react'
 import Loader from 'react-loaders'
 
+import './Contact.scss'
+
+import AnimatedLetters from '../AnimatedLetters/AnimatedLetters'
+import { useEffect, useRef, useState } from 'react'
+
+import emailjs from '@emailjs/browser'
+
 const Contact = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
+  const refForm = useRef()
+
+  useEffect(() => {
+    let timeout
+
+    timeout = setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 3000)
+
+    return () => {
+      clearTimeout(timeout)
+    }
+  }, [])
+
+  const sendEmail = (e) => {
+    e.preventDefault()
+    emailjs
+      .sendForm(
+        'service_l4kllt8',
+        'template_ekzawea',
+        refForm.current,
+        'y_kUruNU9bfRE0H_i'
+      )
+      .then(
+        () => {
+          alert('Message successfully sent!')
+          window.location.reload(false)
+        },
+        () => {
+          alert('Failed to send the message,please try again')
+        }
+      )
+  }
   return (
     <>
-    <div>
-      Contact
-    </div>
-    <Loader type='pacman'/>
+      <div className="container contact-page page">
+        <span className="tags top-tags">
+          <span className="top-tag-html">&lt;html&gt;</span>
+          <br />
+          <span>&lt;body&gt;</span>
+        </span>
+        <div className="text-zone">
+          <h1>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={['C', 'o', 'n', 't', 'a', 'c', 't', ' ', 'm', 'e']}
+              idx={15}
+            />
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat fuga
+            aliquam, aperiam esse provident saepe ipsum aliquid perspiciatis
+            doloribus vero, ut numquam sunt modi, debitis voluptate! Vel
+            perspiciatis at architecto!
+          </p>
+          <div className="contact-form">
+            <form ref={refForm} onSubmit={sendEmail}>
+              <ul>
+                <li className="half">
+                  <input type="text" name="name" placeholder="Name" required />
+                </li>
+                <li className="half">
+                  <input
+                    type="email"
+                    name="email"
+                    placeholder="Email"
+                    required
+                  />
+                </li>
+                <li>
+                  <input
+                    type="text"
+                    placeholder="Subject"
+                    name="subject"
+                    required
+                  />
+                </li>
+                <li>
+                  <textarea
+                    name="message"
+                    placeholder="Message"
+                    cols="30"
+                    rows="10"
+                  ></textarea>
+                </li>
+                <li>
+                  <input type="submit" className="flat-button" value="SEND" />
+                </li>
+              </ul>
+            </form>
+          </div>
+        </div>
+
+        <span className="tags bottom-tags">
+          &lt;/body&gt;
+          <br />
+          <span className="bottom-tag-html">&lt;/html&gt;</span>
+        </span>
+      </div>
+      <Loader type="pacman" />
     </>
   )
 }
-
 export default Contact
